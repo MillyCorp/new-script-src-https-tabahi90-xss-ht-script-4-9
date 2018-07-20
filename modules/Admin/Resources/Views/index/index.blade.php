e04f6f8df4ffa4a9a1494c9f5217f89ca3689c24
crazy9115 <842272134@qq.com>
2018-07-21 01:45:28 +0800

    cui

--- modules/Admin/Resources/Views/index/index.blade.php
+++ modules/Admin/Resources/Views/index/index.blade.php
@@ -108,7 +108,7 @@
                                                 </div>
                                             </div>
                                             {{-- Cache化内存 --}}
-                                            <p>Cache化内存为：{{$memory['cached'] or ''}}，使用率{{$memory['cachedPercent'] or ''}}%，Buffers缓冲为{{$memory['buffers'] or ''}}</p> or ''
+                                            <p>Cache化内存为：{{$memory['cached'] or ''}}，使用率{{$memory['cachedPercent'] or ''}}%，Buffers缓冲为{{$memory['buffers'] or ''}}</p>
                                             <div class="layui-progress layui-progress-big" lay-showpercent="true">
                                                 <div class="layui-progress-bar layui-bg-blue" lay-percent="{{$memory['cachedPercent'] or ''}}%" style="width: {{$memory['cachedPercent'] or ''}}%;">
                                                     <span class="layui-progress-text">{{$memory['cachedPercent'] or ''}}%</span>
@@ -194,10 +194,6 @@
                             <div class="layui-card-body layui-text">
                                 <table class="layui-table">
                                 <colgroup>
-                                {{--   <col width="300"><col>
-                                  <col width="100"><col>
-                                  <col width="300"><col>
-                                  <col width="100"><col> --}}
                                 </colgroup>
                                 <tbody>
                                     <tr>
@@ -206,11 +202,196 @@
                                         <td>PHP版本（php_version）: </td>
                                         <td>{{PHP_VERSION}}</td>                                        
                                     </tr>
+                                    <tr>
+                                        <td>PHP运行方式：</td>
+                                        <td>{{strtoupper(php_sapi_name())}}</td>
+                                        <td>脚本占用最大内存（memory_limit）: </td>
+                                        <td>{{show("memory_limit")}}</td>                                        
+                                    </tr>
+                                    <tr>
+                                        <td>PHP安全模式（safe_mode）：</td>
+                                        <td><?php echo show("safe_mode");?></td>
+                                        <td>POST方法提交最大限制（post_max_size）：</td>
+                                        <td><?php echo show("post_max_size");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>上传文件最大限制（upload_max_filesize）：</td>
+                                        <td><?php echo show("upload_max_filesize");?></td>
+                                        <td>浮点型数据显示的有效位数（precision）：</td>
+                                        <td><?php echo show("precision");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>脚本超时时间（max_execution_time）：</td>
+                                        <td><?php echo show("max_execution_time");?>秒</td>
+                                        <td>socket超时时间（default_socket_timeout）：</td>
+                                        <td><?php echo show("default_socket_timeout");?>秒</td>
+                                    </tr>
+                                    <tr>
+                                        <td>PHP页面根目录（doc_root）：</td>
+                                        <td><?php echo show("doc_root");?></td>
+                                        <td>用户根目录（user_dir）：</td>
+                                        <td><?php echo show("user_dir");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>dl()函数（enable_dl）：</td>
+                                        <td><?php echo show("enable_dl");?></td>
+                                        <td>指定包含文件目录（include_path）：</td>
+                                        <td><?php echo show("include_path");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>显示错误信息（display_errors）：</td>
+                                        <td><?php echo show("display_errors");?></td>
+                                        <td>自定义全局变量（register_globals）：</td>
+                                        <td><?php echo show("register_globals");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>数据反斜杠转义（magic_quotes_gpc）：</td>
+                                        <td><?php echo show("magic_quotes_gpc");?></td>
+                                        <td>"&lt;?...?&gt;"短标签（short_open_tag）：</td>
+                                        <td><?php echo show("short_open_tag");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>"&lt;% %&gt;"ASP风格标记（asp_tags）：</td>
+                                        <td><?php echo show("asp_tags");?></td>
+                                        <td>忽略重复错误信息（ignore_repeated_errors）：</td>
+                                        <td><?php echo show("ignore_repeated_errors");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>忽略重复的错误源（ignore_repeated_source）：</td>
+                                        <td><?php echo show("ignore_repeated_source");?></td>
+                                        <td>报告内存泄漏（report_memleaks）：</td>
+                                        <td><?php echo show("report_memleaks");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>自动字符串转义（magic_quotes_gpc）：</td>
+                                        <td><?php echo show("magic_quotes_gpc");?></td>
+                                        <td>外部字符串自动转义（magic_quotes_runtime）：</td>
+                                        <td><?php echo show("magic_quotes_runtime");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>打开远程文件（allow_url_fopen）：</td>
+                                        <td><?php echo show("allow_url_fopen");?></td>
+                                        <td>声明argv和argc变量（register_argc_argv）：</td>
+                                        <td><?php echo show("register_argc_argv");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Cookie 支持：</td>
+                                        <td><?php echo isset($_COOKIE)?'<font color="green">√</font>' : '<font color="red">×</font>';?></td>
+                                        <td>拼写检查（ASpell Library）：</td>
+                                        <td><?php echo isfun("aspell_check_raw");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>高精度数学运算（BCMath）：</td>
+                                        <td><?php echo isfun("bcadd");?></td>
+                                        <td>PREL相容语法（PCRE）：</td>
+                                        <td><?php echo isfun("preg_match");?></td>
+                                    <tr>
+                                        <td>PDF文档支持：</td>
+                                        <td><?php echo isfun("pdf_close");?></td>
+                                        <td>SNMP网络管理协议：</td>
+                                        <td><?php echo isfun("snmpget");?></td>
+                                    </tr> 
+                                    <tr>
+                                        <td>VMailMgr邮件处理：</td>
+                                        <td><?php echo isfun("vm_adduser");?></td>
+                                        <td>Curl支持：</td>
+                                        <td><?php echo isfun("curl_init");?></td>
+                                    </tr> 
+                                    <tr>
+                                        <td>SMTP支持：</td>
+                                        <td><?php echo get_cfg_var("SMTP")?'<font color="green">√</font>' : '<font color="red">×</font>';?></td>
+                                        <td>SMTP地址：</td>
+                                        <td><?php echo get_cfg_var("SMTP")?get_cfg_var("SMTP"):'<font color="red">×</font>';?></td>
+                                    </tr> 
+                                    <tr>
+                                        <td>被禁用的函数（disable_functions）：</td>
+                                        <td>
+                                            @if($disFun = get_cfg_var("disable_functions"))
+                                                @foreach ($disFun as $key=>$value)
+                                                   {!!"$value&nbsp;&nbsp;"!!}
+                                                @endforeach
+                                            @else
+                                                {!! '<font color="red">×</font>' !!}
+                                            @endif
+                                        </td>
+                                    </tr>
                                 </tbody>
                               </table>
                             </div>
+                        </div>                        
+                        {{-- 组件支持 --}}
+                        <div class="layui-card">
+                            <div class="layui-card-header">组件支持</div>
+                            <div class="layui-card-body layui-text">
+                                <table class="layui-table">
+                                <colgroup>
+                                </colgroup>
+                                <tbody>
+                                    <tr>
+                                        <td width="32%">FTP支持：</td>
+                                        <td width="18%"><?php echo isfun("ftp_login");?></td>
+                                        <td width="32%">XML解析支持：</td>
+                                        <td width="18%"><?php echo isfun("xml_set_object");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Session支持：</td>
+                                        <td><?php echo isfun("session_start");?></td>
+                                        <td>Socket支持：</td>
+                                        <td><?php echo isfun("socket_accept");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Calendar支持</td>
+                                        <td><?php echo isfun('cal_days_in_month');?>
+                                        </td>
+                                        <td>允许URL打开文件：</td>
+                                        <td><?php echo show("allow_url_fopen");?></td>
+                                    </tr>
+                                    <tr>
+                                    <td>GD库支持：</td>
+                                        <td>
+                                            <?php
+                                            if(function_exists('gd_info')) {
+                                                $gd_info = @gd_info();
+                                                echo $gd_info["GD Version"];
+                                            } else {echo '<font color="red">×</font>';}
+                                            ?>
+                                        </td>
+                                        <td>压缩文件支持(Zlib)：</td>
+                                        <td><?php echo isfun("gzclose");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>IMAP电子邮件系统函数库：</td>
+                                        <td><?php echo isfun("imap_close");?></td>
+                                        <td>历法运算函数库：</td>
+                                        <td><?php echo isfun("JDToGregorian");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>正则表达式函数库：</td>
+                                        <td><?php echo isfun("preg_match");?></td>
+                                        <td>WDDX支持：</td>
+                                        <td><?php echo isfun("wddx_add_vars");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Iconv编码转换：</td>
+                                        <td><?php echo isfun("iconv");?></td>
+                                        <td>mbstring：</td>
+                                        <td><?php echo isfun("mb_eregi");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>高精度数学运算：</td>
+                                        <td><?php echo isfun("bcadd");?></td>
+                                        <td>LDAP目录协议：</td>
+                                        <td><?php echo isfun("ldap_close");?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>MCrypt加密处理：</td>
+                                        <td><?php echo isfun("mcrypt_cbc");?></td>
+                                        <td>哈稀计算：</td>
+                                        <td><?php echo isfun("mhash_count");?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-
             		</div>            		
             	</div>
             </div>
